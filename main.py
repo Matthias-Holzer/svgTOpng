941d3fc51ee6af015d1879decfc41b5b566899bd
Matthias-Holzer <wien.matthias.holzer@gmail.com>
2021-08-04 00:06:54 +0200

    path redundency removed

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ path='/Applications/Inkscape.app/Contents/MacOS/inkscape'
 
 import os
 
-os.system("/Applications/Inkscape.app/Contents/MacOS/inkscape 'svg/1fac0.svg' -w 128 -h 128 -o 'png128/1fac0.png'")
-os.system("/Applications/Inkscape.app/Contents/MacOS/inkscape 'svg/1fac0.svg' -w 512 -h 512 -o 'png512/1fac0.png'")
+os.system(path + " 'svg/1fac0.svg' -w 128 -h 128 -o 'png128/1fac0.png'")
+os.system(path + " 'svg/1fac0.svg' -w 512 -h 512 -o 'png512/1fac0.png'")
