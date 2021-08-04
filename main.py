fb91352ff64c0ed4157d5c65aa9a9fe9db0bb1b3
Matthias-Holzer <wien.matthias.holzer@gmail.com>
2021-08-04 02:13:30 +0200

    functunal, cleaned, testet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import time
+start = time.time()
 
 x=512
 y=512
@@ -17,8 +19,9 @@ for file in files:
     print(file)
     os.system(path + " " +file+ " -w "+str(x)+" -h "+str(y)+" -o "+ new_folder + file.split("svg/")[1].split(".")[0] +".png")
 
-#path for output
-path_out = "~/PycharmProjects/imgResize/"
 
 #os.system(path + " 'svg/1fac0.svg' -w 128 -h 128 -o 'png128/1fac0.png'")
 #os.system(path + " 'svg/1fac0.svg' -w 512 -h 512 -o 'png512/1fac0.png'")
+
+end = time.time()
+print("The Program took "+str(end-start)+"seconds to convert all the files")
